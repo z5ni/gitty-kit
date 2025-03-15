4071ab59dd39cc27411cdd751e9b432e33f8ce26
zion <0o0w0d@gmail.com>
2025-03-15 22:36:04 +0900

    update config handling

--- gitty_kit/ai_client.py
+++ gitty_kit/ai_client.py
@@ -1,21 +1,28 @@
 import os
 from openai import OpenAI
-from config import DEFAULT_CONFIG
+from config import CONFIG_DIR, CONFIG_FILE
+from setup_manager import config_load
 from message_generator import prepare_prompt
 from utils.git import git_diff
 
+# 설정 파일에서 값 가져오기
+config = config_load()
 
 client = OpenAI(
-    api_key=DEFAULT_CONFIG["api"]["key"],
+    api_key=config["api"]["key"],
 )
+
+model = config["api"]["model"]
+language = config["language"]
+style = config["format"]["style"]
 
 # instructions: 모델의 행동을 결정하는 프롬프트
 # input:  모델이 작업을 수행하기 위해 필요한 추가 데이터
 response = client.responses.create(
-    model=DEFAULT_CONFIG["api"]["model"],
+    model=model,
     instructions=prepare_prompt(
-        style="simple",
-        lang="en",
+        style=style,
+        lang=language,
         max_length=50,
     ),
     input=git_diff(),
